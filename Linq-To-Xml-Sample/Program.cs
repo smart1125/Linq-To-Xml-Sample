46debb923c4b5a9cd2d83838a6ea1c75f2270578
BAIJIAHAO <white@changingtec.com>
2019-10-15 13:52:24 +0800

    [change] 使用 Markdown 寫 README 檔案
    使用Markdown file 記錄重點，目前看起來比較清楚且方便。

--- Linq-To-Xml-Sample/Program.cs
+++ Linq-To-Xml-Sample/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Linq_To_Xml_Sample
 {
     class Program
     {
+        static XElement CreateFileSystemXmlTree(string source)
+        {
+            DirectoryInfo di = new DirectoryInfo(source);
+            return new XElement("Dir",
+                new XAttribute("Name", di.Name),
+                from d in Directory.GetDirectories(source)
+                select CreateFileSystemXmlTree(d),
+                from fi in di.GetFiles()
+                select new XElement("File",
+                    new XElement("Name", fi.Name),
+                    new XElement("Length", fi.Length)
+                )
+            );
+        }
+
+
         static void Main(string[] args)
         {
+            XElement fileSystemTree = CreateFileSystemXmlTree("C:/Tmp");
+            Console.WriteLine(fileSystemTree);
+            Console.WriteLine("------");
+            long totalFileSize =
+                (from f in fileSystemTree.Descendants("File")
+                 select (long)f.Element("Length")).Sum();
+            Console.WriteLine("Total File Size:{0}", totalFileSize);
+
+
         }
     }
 }
---
+++ Linq-To-Xml-Sample/zh-tw/README.md
@@ -0,0 +1,23 @@
+﻿# Linq To Xml
+
+----
+## 說明
+see [MSDN]
+(https://docs.microsoft.com/zh-tw/dotnet/csharp/programming-guide/concepts/linq/linq-to-xml-overview)
+
+> 整理 MSDN 上面有關 Linq To Xml 教學重點 。
+> 
+>旅程開始：
+----
+* [使用者入門](01.md)
+* [LINQ to XML 程式設計概觀](02.md)
+* [XML 樹狀結構]()
+* [使用 XML 命名空間]()
+* [序列化 XML 樹狀結構]()
+* [LINQ to XML 軸]()
+* [查詢 XML 樹狀]()
+* [修改 XML 樹狀結構]()
+* [效能]()
+* [進階 LINQ to XML 程式設計]()
+* [LNQ to XML 安全性]()
+* [範例 XML 文件]()
